113866b84befd3040dcc40fc5b5b3be91a1bb623
Nuraida <nuraaidnns@gmail.com>
2025-07-25 16:52:22 +0600

    footer style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} Tech Store. Все права защищены.</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
